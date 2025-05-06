f73de1150300c7795a920a95c7ba4c08feddf3d1
Wilson Owino <37038018+Wilson-Ochieng@users.noreply.github.com>
2025-05-06 06:28:44 +0300

    Used inkwell widget to add Tappable effect

--- lib/widgets/category_grid_item.dart
+++ lib/widgets/category_grid_item.dart
@@ -8,24 +8,35 @@ class CategoryGridItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return InkWell(
 
-      padding: const EdgeInsets.all(16) ,
-      decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [
-        category.color.withOpacity(0.55),
-        category.color.withOpacity(0.9)
-      ],
+      onTap: () {
+
+
+
+      },
+      splashColor:Theme.of(context).primaryColor ,
+      borderRadius: BorderRadius.circular(16),
+      child: Container(
       
-      begin: Alignment.topLeft,
-      end: Alignment.bottomRight,
-    
-      )),
-      child: Text(category.title,
-      style:Theme.of(context).textTheme.titleLarge!.copyWith(
-        color: Theme.of(context).colorScheme.onBackground,
-
-      ) ,),
+        padding: const EdgeInsets.all(16) ,
+        decoration: BoxDecoration(
+          borderRadius:BorderRadius.circular(16),
+            gradient: LinearGradient(colors: [
+          category.color.withOpacity(0.55),
+          category.color.withOpacity(0.9)
+        ],
+        
+        begin: Alignment.topLeft,
+        end: Alignment.bottomRight,
+      
+        )),
+        child: Text(category.title,
+        style:Theme.of(context).textTheme.titleLarge!.copyWith(
+          color: Theme.of(context).colorScheme.onBackground,
+      
+        ) ,),
+      ),
     );
   }
 }
