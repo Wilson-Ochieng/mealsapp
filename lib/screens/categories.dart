e100fb1548d026f255648ef089ed2f58865d45c7
Wilson Owino <37038018+Wilson-Ochieng@users.noreply.github.com>
2025-05-06 13:03:25 +0300

    passed categories using category id

--- lib/screens/categories.dart
+++ lib/screens/categories.dart
@@ -1,14 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:mealsapp/data/dummy_data.dart';
+import 'package:mealsapp/model/category.dart';
 import 'package:mealsapp/screens/meals.dart';
 import 'package:mealsapp/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context,Category category) {
+
+   final filteredMeals = dummyMeals.where((meal) => meal.categories.contains(category.id)).toList();
+
     Navigator.of(context).push(MaterialPageRoute(
-      builder: (ctx) => const MealsScreen(title: 'some title', meals: []),
+      builder: (ctx) => MealsScreen(title: category.title, meals:filteredMeals ),
     )); //Navigator.push(context,route)
   }
 
@@ -33,7 +37,7 @@ class CategoriesScreen extends StatelessWidget {
             CategoryGridItem(
               category: category,
               onSelectCategory: () {
-                _selectCategory(context);
+                _selectCategory(context,category);
               },
             )
         ],
